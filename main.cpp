8a891cb60c82c223e40b0aa3121b38e1765ef1e6
ジョカエイ <147121578+SenriChoi@users.noreply.github.com>
2024-06-21 15:03:07 +0900

    04_03_確認課題

--- main.cpp
+++ main.cpp
@@ -5,15 +5,17 @@
 
 
 int main() {
+	//作成
 	SeaAnimal* seaAnimal[2];
 
 	seaAnimal[0] = new JellyFish();
 	seaAnimal[1] = new Dolphin();
 
-	
+	//Print関数呼び出し
 	seaAnimal[0]->Print();
 	seaAnimal[1]->Print();
 
+	//削除
 	delete seaAnimal[0];
 	delete seaAnimal[1];
 
