b72cc71129afb1938df4ce45f1050cfaffff5822
ジョカエイ <147121578+SenriChoi@users.noreply.github.com>
2024-06-28 14:52:00 +0900

    04_04

--- main.cpp
+++ main.cpp
@@ -3,17 +3,25 @@
 #include "Rectangle.h"
 
 int main() {
+    // IShape型のポインタ配列
+    IShape* shapes[2];
 
     // Circle作成
-    Circle circle(5.0f); 
-    circle.Size();        // 面積計算
-    circle.Draw();        // 描画
-
+    shapes[0] = new Circle(5.0f); 
     // Rectangle作成
-    Rectangle rectangle(4.0f, 3.0f); 
-    rectangle.Size();       // 面積計算
-    rectangle.Draw();       // 描画
+    shapes[1] = new Rectangle(4.0f, 3.0f);
 
+
+
+    for (int i = 0; i < 2; ++i) {
+        shapes[i]->Size();  // 面積計算
+        shapes[i]->Draw();  // 描画
+    }
+
+
+    for (int i = 0; i < 2; ++i) {
+        delete shapes[i];  //解放
+    }
 
 	return 0;
 }
